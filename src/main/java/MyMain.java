3ef447dd2162ca87cbde388569c16338263c4184
gunner10z-school <24jzweiback@athenian.org>
2021-09-14 10:03:52 -0700

    first

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,7 +4,9 @@ public class MyMain {
     // Ex. divisibleByFour(16) -> true
     //     divisibleByFour(5) -> false
     public static boolean divisibleByFour(int num) {
-        // REPLACE WITH YOUR CODE HERE
+        if (num%4==0){
+            return true;
+        }
         return false;
     }
 
@@ -14,15 +16,26 @@ public class MyMain {
     // 6 7 8 9 10
     // ...
     public static void printNumsUpTo(int biggest) {
-        // REPLACE THIS WITH YOUR CODE
+        for (int i=1;i<=biggest;i++){
+            if ((i-1)%5==0){
+                System.out.println();
+            }
+            System.out.print(i);
+        }
     }
 
     // This method is given an input x, and it adds up the sum of its digits
     // Ex. sumDigits(61) -> 7 because 6 + 1 = 7
     //     sumDigits(123) -> 6 because 1 + 2 + 3 = 6
     public static int sumDigits(int x) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int totalLastDigit=0;
+        while(x>0){
+            int lastDigit=x%10;
+            //System.out.println(lastDigit);
+            totalLastDigit+=lastDigit;
+            x=x/10;
+        }
+        return totalLastDigit;
     }
 
     // Bonus: This method returns true if the input num is a "self-divisor",
@@ -35,8 +48,18 @@ public class MyMain {
     // Note that 0 is not considered to be a divisor of any number, so any number containing a 0
     // digit is NOT a self-divisor.
     public static boolean isSelfDivisor(int num) {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        int newNum=num;
+        while(num>0){
+            int lastDigit=num%10;
+            if (lastDigit==0){
+                return false;
+            }
+            if (newNum%lastDigit!=0){
+                return false;
+            }
+            num=num/10;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
